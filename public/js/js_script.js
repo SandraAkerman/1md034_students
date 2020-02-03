2483701a063cc398e15bb8c8bf48a998356dfa15
SandraAkerman <sandra.akerman.6464@student.uu.se>
2020-02-03 16:55:13 +0100

    4-7 done

--- public/js/js_script.js
+++ public/js/js_script.js
@@ -1,25 +1,26 @@
-function menuItem(nm, cal, mo, al, img) {
+function menuItem(nm, cal, mo, gl, hz, img) {
   this.name = nm;
   this.kcal = cal;
   this.madeOf = mo;
-  this.allergens = al;
+  this.gluten = gl;
+  this.hazelnuts = hz
   this.imagePath = img;
   this.nameAndKcal = function(){
     return this.name + ' ' + this.kcal;
 };
 }
 
-let burger1 = new menuItem("Smoky burger", "500kCal","patty made from black beans and sweet potatoes","contains gluten");
+let burger1 = new menuItem("Smoky burger", "500kCal","patty made from black beans and sweet potatoes",true, false, "");
 
-let burger2 = new menuItem("California burger", "550kCal","patty made from carrots and quick oats", "contains gluten");
+let burger2 = new menuItem("California burger", "550kCal","patty made from carrots and quick oats", true, false, "");
 
-let burger3 = new menuItem("Brooklyn burger", "600kCal","patty made from black beans", "contains gluten \n contains hazelnuts");
+let burger3 = new menuItem("Brooklyn burger", "600kCal","patty made from black beans", true, true, "");
 
-let burger4 = new menuItem("Amazing burger", "600kCal","patty made from....", "contains gluten \n contains hazelnuts");
+let burger4 = new menuItem("Amazing burger", "600kCal","patty made from....", true, false, "");
 
-let burger5 = new menuItem("Healthy burger", "450kCal","patty made from...");
+let burger5 = new menuItem("Healthy burger", "450kCal","patty made from...", false, false, "");
 
-
+/*
 var burgerOne = document.createElement("p");
 burgerOne.innerHTML = burger1.nameAndKcal();
 document.getElementById("myID").appendChild(burgerOne);
@@ -39,6 +40,24 @@ document.getElementById("myID").appendChild(burgerFour);
 var burgerFive = document.createElement("p");
 burgerFive.innerHTML = burger5.nameAndKcal();
 document.getElementById("myID").appendChild(burgerFive);
+*/
+
+var menu = [burger1, burger2, burger3];
+
+let i = 0;
+for (i; i < menu.length; i++) {
+	var burger = document.createElement("p");
+	burger.innerHTML = menu[i].nameAndKcal();
+
+  if(menu[i].gluten) {
+		burger.innerHTML = burger.innerHTML + " " + "contains gluten";
+    }
+
+  if(menu[i].hazelnuts){
+		 burger.innerHTML = burger.innerHTML + " " +"contains hazelnuts";
+     }
+	document.getElementById("myID").appendChild(burger);
+}
 
 
 
--- public/js/vue_script.js
+++ public/js/vue_script.js
@@ -1,19 +1,51 @@
-document.getElementById("myID").innerHTML = "Välj en burgare";
+/* HTML for Vue*/
+<script src="https://vuejs.org/js/vue.js"></script>
+
+
+<div id="myID">
+    <h1>Välj en burgare</h1>
+    <div v-for="burger in menu">
+   {{ burger.name }}
+
+    <span v-if="burger.gluten" >
+    contains gluten
+    </span>
+      <span v-if="burger.hazelnuts" >
+    contains hazelnuts
+    </span>
+   </div>
+
+-------
+
+function menuItem(nm, cal, mo, gl, hz, img) {
+  this.name = nm;
+  this.kcal = cal;
+  this.madeOf = mo;
+  this.gluten = gl;
+  this.hazelnuts = hz
+  this.imagePath = img;
+  this.nameAndKcal = function(){
+    return this.name + ' ' + this.kcal;
+};
+}
+
+let burger1 = new menuItem("Smoky burger", "500kCal","patty made from black beans and sweet potatoes",true, false, "");
+
+let burger2 = new menuItem("California burger", "550kCal","patty made from carrots and quick oats", true, false, "");
+
+let burger3 = new menuItem("Brooklyn burger", "600kCal","patty made from black beans", true, true, "");
+
+let burger4 = new menuItem("Amazing burger", "600kCal","patty made from....", true, false, "");
+
+let burger5 = new menuItem("Healthy burger", "450kCal","patty made from...", false, false, "");
+
+
+var menu = [burger1, burger2, burger3, burger4, burger5];
+
 
 const vm = new Vue({
   el: '#myID',
   data: {
-    arbitraryVariableName: 'Välj en burgare'
-  }
-})
-
-const vm = new Vue({
-  el: '#myID',
-  data: {
-    burger1: burger1.nameAndKcal(),
-    burger2: burger2.nameAndKcal(),
-    burger3: burger3.nameAndKcal(),
-    burger4: burger4.nameAndKcal(),
-    burger5: burger5.nameAndKcal()
+  	menu: menu,
   }
 })
